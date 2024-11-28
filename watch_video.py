ae77076918b963d7683114944e73e6446775024f
Noam Cohen <cohen.noam@hotmail.com>
2024-11-28 12:50:38 -0500

    added watch_video file

--- watch_video.py
+++ watch_video.py
@@ -3,14 +3,34 @@ import cv2
 
 filename = "Data/box/depth.npy"
 filename = "Data/lama/depth.npy"
-filename = "Data/mold/depth.npy"
-filename = "Data/puzzle/depth.npy"
+# filename = "Data/mold/depth.npy"
+# filename = "Data/puzzle/depth.npy"
+# filename = "Data/puzzle2/depth.npy"
+# filename = "Data/shoe/depth.npy"
+# filename = "Data/ukulele/depth.npy"
 filename = "Data/ukulele2/depth.npy"
-filename = "Data/shoe/depth.npy"
-data = np.load(filename)
 
-for i in range(data.shape[0]):
-    frame = data[i]
+depth_frames = np.load(filename)
+# print(depth_frames[100, 5])
+
+# Define the depth range for lama
+lower_bound = 500
+upper_bound = 650
+
+# Define the depth range for ukulele
+lower_bound = 470
+upper_bound = 850
+
+# Apply the range filter
+depth_frames[(depth_frames < lower_bound) | (depth_frames > upper_bound)] = 0
+
+for i in range(depth_frames.shape[0]):
+    # for lama
+    # frame = depth_frames[i, 50:150, 100:200]
+
+    # for ukulele
+    frame = depth_frames[i, 30:200, 50:300]
+
     # Normalize the frame to the range [0, 255] and convert to uint8
     frame_normalized = cv2.normalize(frame, None, 0, 255, cv2.NORM_MINMAX)
     frame_normalized = frame_normalized.astype("uint8")
@@ -21,6 +41,7 @@ for i in range(data.shape[0]):
     )
 
     # Display the frame
+    # print(i)
     cv2.imshow("Animation", resized_frame)
     if cv2.waitKey(50) & 0xFF == 27:  # 50ms delay, press ESC to exit
         break
